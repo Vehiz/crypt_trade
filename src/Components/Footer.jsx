0def74ecb506f6b308e5f8f8f83777b7c555b24d
Vehiz <113064866+Vehiz@users.noreply.github.com>
2023-10-11 00:30:26 +0100

    footer

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,21 @@
+// import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+        <div className="pt-[70px] pb-[100px]">
+            <div>
+                <div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+            </div>
+        </div>
+        <div></div>
+    </footer>
+  )
+}
+
+export default Footer
